c644ab7da7dabe58a724422fa0a9aa5d9107560a
ArturKaminskiSzczecin <a_rudnicki@yahoo.com>
2021-07-22 11:49:45 +0200

    poprawione, nie dziala

--- morse.cpp
+++ morse.cpp
@@ -3,21 +3,23 @@
 #include<algorithm>
 #include<vector>
 std::vector<std::string> morse  = {".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--..", "-----", ".----", "..---", "...--", "....-", ".....", "-....", "--...", "---..", "----.", ".-.-.-", "--..--", "..--..", ".----.", "-.-.--", "-..-.", "-.--.", "-.--.-", ".-...", "---...", "-.-.-.", "-...-", ".-.-.", "-....-", "..--.-", ".-..-.", "...-..-", ".--.-.", "...---..."};  
-std::string ascii [] = {"A",  "B",    "C",    "D",   "E", "F",    "G",   "H",    "I",  "J",    "K",   "L",    "M",  "N",  "O",   "P",    "Q",    "R",   "S",   "T", "U",   "V",    "W",   "X",    "Y",    "Z",    "0",     "1",     "2",     "3",     "4",     "5",     "6",     "7",     "8",     "9",     ".",      ",",      "?",      "'",      "!",      "/",     "(",     ")",      "&",     ":",      ";",      "=",     "+",     "-",      "_",      "\"",     "$",       "@",      "SOS"};
+std::vector<char> ascii  = {"A",  "B",    "C",    "D",   "E", "F",    "G",   "H",    "I",  "J",    "K",   "L",    "M",  "N",  "O",   "P",    "Q",    "R",   "S",   "T", "U",   "V",    "W",   "X",    "Y",    "Z",    "0",     "1",     "2",     "3",     "4",     "5",     "6",     "7",     "8",     "9",     ".",      ",",      "?",      "'",      "!",      "/",     "(",     ")",      "&",     ":",      ";",      "=",     "+",     "-",      "_",      "\"",     "$",       "@",      "SOS"};
 
 std::string decodeMorse(std::string morseCode) {
     // ToDo: Accept dots, dashes and spaces, return human-readable message
+    std::string temp;
     auto n = morse.begin();
     std::string decoded;
-    for( auto p : morseCode){
+    for( const char p : morseCode){
       
       if (p == ' '){
-        
+        n = std::find(morse.begin(), morse.end(), temp);
+        decoded.push_back(ascii[n];
+        temp.erase(temp.begin(), temp.end());
         std::cout << '\n';
       }
       else{
-         n = std::find(morse.begin(), morse.end(), p);
-        decoded.push_back(ascii[n]);
+          temp.push_back(p);
       }
     }
     std::cout << "\n";
