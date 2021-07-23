6ea40c907c54c91c503942097c0ebeee36a9cdd9
ArturKaminskiSzczecin <a_rudnicki@yahoo.com>
2021-07-23 10:01:35 +0200

    it works with std::distance

--- morse.cpp
+++ morse.cpp
@@ -2,6 +2,7 @@
 #include <cstring>
 #include<algorithm>
 #include<vector>
+#include<map>
 std::vector<std::string> morse  = {".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--..", "-----", ".----", "..---", "...--", "....-", ".....", "-....", "--...", "---..", "----.", ".-.-.-", "--..--", "..--..", ".----.", "-.-.--", "-..-.", "-.--.", "-.--.-", ".-...", "---...", "-.-.-.", "-...-", ".-.-.", "-....-", "..--.-", ".-..-.", "...-..-", ".--.-.", "...---..."};  
 std::vector<std::string> ascii  = {"A",  "B",    "C",    "D",   "E", "F",    "G",   "H",    "I",  "J",    "K",   "L",    "M",  "N",  "O",   "P",    "Q",    "R",   "S",   "T", "U",   "V",    "W",   "X",    "Y",    "Z",    "0",     "1",     "2",     "3",     "4",     "5",     "6",     "7",     "8",     "9",     ".",      ",",      "?",      "'",      "!",      "/",     "(",     ")",      "&",     ":",      ";",      "=",     "+",     "-",      "_",      "\"",     "$",       "@",      "SOS"};
 
@@ -10,13 +11,19 @@ std::string decodeMorse(std::string morseCode) {
     std::string temp;
     //size_t n = morse.begin();
     std::string decoded;
-    for( auto p : morseCode){
+    /*std::map<std::string, std::string> myMorse;
+    std::transform(morse.begin(),morse.end(),ascii.begin(),std::inserter(myMorse, myMorse.end()), [] (std::string a, std::string b){
+       std::make_pair(a,b);
+    });
+    */
+   for( auto p : morseCode){
       
       if (p == ' '){
         auto n = std::find(morse.begin(), morse.end(), temp);
-        decoded.push_back(ascii[n]);
+        int pos = std::distance(morse.begin(), n);
+        decoded.append(ascii[pos]);
         temp.erase(temp.begin(), temp.end());
-        std::cout << '\n';
+    
       }
       else{
           temp.push_back(p);
@@ -29,6 +36,6 @@ int main(){
     std::string OSO = {"--- ... ---   .- .-."};
 
   std::cout << "decoded \n";
-  std::cout << 	decodeMorse(OSO) << '\n';
+  std::cout << decodeMorse(OSO) << '\n';
   //printf("%s\n", decode_morse(OSO));
 }
