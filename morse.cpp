a245117e53ef874c7c2f94d92f808eafb1d0f0e0
Artur Rudnicki <art.rudnicki@gmail.com>
2021-07-30 11:31:14 +0200

    Need some advice

--- morse.cpp
+++ morse.cpp
@@ -1,39 +1,38 @@
 #include <iostream>
 #include <cstring>
-#include<algorithm>
+#include <algorithm>
 #include<vector>
-#include<map>
 std::vector<std::string> morse  = {".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--..", "-----", ".----", "..---", "...--", "....-", ".....", "-....", "--...", "---..", "----.", ".-.-.-", "--..--", "..--..", ".----.", "-.-.--", "-..-.", "-.--.", "-.--.-", ".-...", "---...", "-.-.-.", "-...-", ".-.-.", "-....-", "..--.-", ".-..-.", "...-..-", ".--.-.", "...---..."};  
 std::vector<std::string> ascii  = {"A",  "B",    "C",    "D",   "E", "F",    "G",   "H",    "I",  "J",    "K",   "L",    "M",  "N",  "O",   "P",    "Q",    "R",   "S",   "T", "U",   "V",    "W",   "X",    "Y",    "Z",    "0",     "1",     "2",     "3",     "4",     "5",     "6",     "7",     "8",     "9",     ".",      ",",      "?",      "'",      "!",      "/",     "(",     ")",      "&",     ":",      ";",      "=",     "+",     "-",      "_",      "\"",     "$",       "@",      "SOS"};
 
-std::string decodeMorse(std::string morseCode) {
-    // ToDo: Accept dots, dashes and spaces, return human-readable message
+std::string decodeMorse(std::string morseCode){
     std::string temp;
-    //size_t n = morse.begin();
     std::string decoded;
-    /*std::map<std::string, std::string> myMorse;
-    std::transform(morse.begin(),morse.end(),ascii.begin(),std::inserter(myMorse, myMorse.end()), [] (std::string a, std::string b){
-       std::make_pair(a,b);
-    });
-    */
-   std::vector<std::string>::iterator n = morse.begin();
-   for( auto p : morseCode){
-      
-      if (p == ' '){
-         if(n != find(morse.begin(), morse.end(), temp)){  //if find will not find temp character will skip adding temp
-             n = std::find(morse.begin(), morse.end(), temp);
-             int pos = std::distance(morse.begin(), n);
-             decoded.append(ascii[pos]);
-             temp.erase(temp.begin(), temp.end());
-         }
-         else
-             std::cout<< "wrong characters\n";
-      }
-      else{
-          temp.push_back(p);
-      }
+    std::vector<std::string>::iterator n = morse.begin();
+    std::cout << "first n: " << &n << "\n";
+    for( auto p : morseCode){
+        //std::cout << p;
+        if (p== ' '){
+            try{ 
+                n = std::find(morse.begin(), morse.end(), temp);
+                if (n == morse.end()){
+                    throw temp;
+                }
+                else{
+                    int pos = std::distance(morse.begin(), n);
+                    decoded.append(ascii[pos]);
+                    temp.erase(temp.begin(), temp.end());
+                }
+            }
+            catch(std::string t){
+                t.erase(temp.begin(), t.end());
+                std::cout << "wrong string\n";
+            }
+        }
+        else{
+            temp.push_back(p);
+        }
     }
-    n = std::find(morse.begin(), morse.end(), temp);
     int pos = std::distance(morse.begin(), n);
     decoded.append(ascii[pos]);
     temp.erase(temp.begin(), temp.end());
@@ -42,9 +41,9 @@ std::string decodeMorse(std::string morseCode) {
 }
 int main(){
     std::string OSO = {" --- ... ---   .- .-."};
-    std::string OSX = {" ssa .- .-. - ..- .-."};
+    std::string OSX = {" .- .-. - ..- .-.    .-."};
 
     std::cout << "decoded \n";
-    //std::cout << decodeMorse(OSO) << '\n';
+    std::cout << decodeMorse(OSO) << '\n';
     std::cout << decodeMorse(OSX) << '\n';
 }
